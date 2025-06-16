0807911435a7f04620bcf7cdde0a9f0661c5fd9b
Jacobo281 <79229379+Jacobo281@users.noreply.github.com>
2025-06-16 10:22:09 -0500

    Add files via upload

---
+++ src/main/java/com/mysaml/clases/java/ClasesJava_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mysaml.clases.java;
+
+/**
+ *
+ * @author diegofmo0802
+ */
+public class ClasesJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
